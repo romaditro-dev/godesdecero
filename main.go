76bd569d0a8798222c3d33eee3344e45ca2c0f84
Rodrigo Diaz Troncoso <sr.rdiazt@gmail.com>
2025-10-15 15:24:59 -0300

    Primer ejercicio

--- main.go
+++ main.go
@@ -3,17 +3,28 @@ package main
 import (
 	"fmt"
 
-	"github.com/romaditro/godesdecero/variables"
+	"github.com/romaditro/godesdecero/ejercicios"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	/*
+		variables.MuestroEnteros()
 
-	variables.RestoVariables()
+		variables.RestoVariables()
 
-	estado, texto := variables.ConviertoaTexto(1588)
+		estado, texto := variables.ConviertoaTexto(1588)
 
-	fmt.Println(estado)
+		fmt.Println(estado)
+		fmt.Println(texto)
+	*/
+
+	numero, texto := ejercicios.ConvNumerico("100")
+	fmt.Println(numero)
+	fmt.Println(texto)
+
+	numero, texto = ejercicios.ConvNumerico("*/*//")
+
+	fmt.Println(numero)
 	fmt.Println(texto)
 
 }
